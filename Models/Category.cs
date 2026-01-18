b7d3b996196827fb956957ad67dfaa7bb12d460c
salesluis <salesluis434@gmail.com>
2026-01-18 11:55:06 -0300

    Feat: Create Models to database

---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+using Dapper.Contrib.Extensions;
+
+namespace Blog.Models;
+[Table("[Role]")]
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Slug { get; set; }
+}
