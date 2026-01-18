8476c2e5d66412569e5098698002ae061c809138
salesluis <salesluis434@gmail.com>
2026-01-18 11:58:02 -0300

    Feat: Add configuration class with database connection string

---
+++ Config/Config.cs
@@ -0,0 +1,7 @@
+namespace Blog.Config;
+
+public static class Config
+{
+    public static string ConnectionString =
+        "Server=localhost,1433;Database=Blog;User Id=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True";
+}
